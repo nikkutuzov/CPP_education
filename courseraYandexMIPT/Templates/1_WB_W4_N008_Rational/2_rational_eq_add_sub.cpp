e23cd49e4db54f21dc6f4a96a637fd4c3f92ed4d
Nikolai Kutuzov <nik.i.kutuzov@yandex.ru>
2022-07-02 20:53:34 +0400

    add Template for 1_WB_W4_008_Rational_part_2

---
+++ courseraYandexMIPT/Templates/1_WB_W4_N008_Rational/2_rational_eq_add_sub.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+class Rational {
+    // Вставьте сюда реализацию класса Rational из первой части
+};
+
+// Реализуйте для класса Rational операторы ==, + и -
+
+int main() {
+    {
+        Rational r1(4, 6);
+        Rational r2(2, 3);
+        bool equal = r1 == r2;
+        if (!equal) {
+            std::cout << "4/6 != 2/3" << std::endl;
+            return 1;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a + b;
+        bool equal = c == Rational(2, 1);
+        if (!equal) {
+            std::cout << "2/3 + 4/3 != 2" << std::endl;
+            return 2;
+        }
+    }
+
+    {
+        Rational a(5, 7);
+        Rational b(2, 9);
+        Rational c = a - b;
+        bool equal = c == Rational(31, 63);
+        if (!equal) {
+            std::cout << "5/7 - 2/9 != 31/63" << std::endl;
+            return 3;
+        }
+    }
+
+    std:cout << "OK" << std::endl;
+  
+    return 0;
+}
