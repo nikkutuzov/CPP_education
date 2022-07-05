3bfcb0bf9ab6d0cbce8ca3413f3bf13d1388b80d
Nikolai Kutuzov <nik.i.kutuzov@yandex.ru>
2022-07-05 10:37:29 +0400

    DONE! W1_N004_RegionsBase - база регионов

--- courseraYandexMIPT/2_YellowBelt/W1_N004_RegionsBase.cpp
+++ courseraYandexMIPT/2_YellowBelt/W1_N004_RegionsBase.cpp
@@ -3,7 +3,7 @@
 /*================================================================================*/
 /*  Title: Regions base                                                           */
 /*  Start_time: 04.07.2022 19:48                                                  */
-/*  End_time:                                                                     */
+/*  End_time: 05.07.2022 10:36                                                    */
 /*  Note:                                                                         */
 /*================================================================================*/
 
@@ -118,6 +118,36 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <tuple>
+
+enum class Lang {
+  DE, FR, IT
+};
+
+struct Region {
+  std::string std_name;
+  std::string parent_std_name;
+  std::map<Lang, std::string> names;
+  int64_t population;
+};
+
+bool operator< (const Region& lhs, const Region& rhs) {
+  return tie(lhs.std_name, lhs.parent_std_name, lhs.names, lhs.population) <
+         tie(rhs.std_name, rhs.parent_std_name, rhs.names, rhs.population);
+}
+
+int FindMaxRepetitionCount(const std::vector<Region>& regions) {
+  int repeat = 0;
+  std::map<Region, int> repeat_regions;
+
+  for (const Region& region : regions) {
+    ++repeat_regions[region];
+    repeat = std::max(repeat, repeat_regions.at(region));
+  }
+
+  return repeat;
+}
 
 /*<-------------------------------------main------------------------------------->*/
 
