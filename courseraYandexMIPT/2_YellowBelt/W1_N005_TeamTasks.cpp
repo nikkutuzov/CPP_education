63e00fa8ca4a1051b67ec7ed3119bb53926281f0
Nikolai Kutuzov <nik.i.kutuzov@yandex.ru>
2022-07-05 10:40:10 +0400

    add W1_N005_teamTasks - трекер задач

---
+++ courseraYandexMIPT/2_YellowBelt/W1_N005_TeamTasks.cpp
@@ -0,0 +1,181 @@
+/*================================================================================*/
+/*                    C++_Development_Basics: The_Yellow_Belt                     */
+/*================================================================================*/
+/*  Title: Team tasks                                                             */
+/*  Start_time: 05.07.2022 10:37                                                  */
+/*  End_time:                                                                     */
+/*  Note:                                                                         */
+/*================================================================================*/
+
+/*<===============================Problem_Statement==============================>*/
+
+/*
+ * Реализуйте класс TeamTasks, позволяющий хранить статистику по статусам
+ * задач команды разработчиков:
+ *
+ * // Перечислимый тип для статуса задачи
+ * enum class TaskStatus {
+ *   NEW,          // новая
+ *   IN_PROGRESS,  // в разработке
+ *   TESTING,      // на тестировании
+ *   DONE          // завершена
+ * };
+ *
+ * // Объявляем тип-синоним для map<TaskStatus, int>,
+ * // позволяющего хранить количество задач каждого статуса
+ * using TasksInfo = map<TaskStatus, int>;
+ *
+ * class TeamTasks {
+ * public:
+ *   // Получить статистику по статусам задач конкретного разработчика
+ *   const TasksInfo& GetPersonTasksInfo(const string& person) const;
+ *
+ *   // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
+ *   void AddNewTask(const string& person);
+ *
+ *   // Обновить статусы по данному количеству задач конкретного разработчика,
+ *   // подробности см. ниже
+ *   tuple<TasksInfo, TasksInfo> PerformPersonTasks(
+ *   const string& person, int task_count);
+ * };
+ *
+ * Метод PerformPersonTasks должен реализовывать следующий алгоритм:
+ *
+ * 1. Рассмотрим все невыполненные задачи разработчика person.
+ * 2. Упорядочим их по статусам: сначала все задачи в статусе NEW, затем все
+ *    задачи в статусе IN_PROGRESS и, наконец, задачи в статусе TESTING.
+ * 3. Рассмотрим первые task_count задач и переведём каждую из них в следующий
+ *    статус в соответствии с естественным порядком:
+ *    NEW -> IN_PROGRESS -> TESTING -> DONE
+ * 4. Вернём кортеж из двух элементов: информацию о статусах обновившихся задач
+ *    (включая те, которые оказались в статусе DONE) и информацию о статусах
+ *    остальных не выполненных задач.
+ *
+ * В случае отсутствия разработчика с именем person метод PerformPersonTasks
+ * должен вернуть кортеж из двух пустых TasksInfo
+ *
+ * Гарантируется, что task_count является положительным числом. Если task_count
+ * превышает текущее количество невыполненных задач разработчика, достаточно
+ * считать, что task_count равен количеству невыполненных задач: дважды
+ * обновлять статус какой-либо задачи в этом случае не нужно.
+ *
+ * Кроме того, гарантируется, что метод GetPersonTasksInfo не будет вызван для
+ * разработчика, не имеющего задач.
+ *
+ * Пример работы метода PerformPersonTasks:
+ *
+ * Предположим, что у конкретного разработчика имеется 10 задач со следующими
+ * статусами:
+ *
+ * NEW - 3
+ * IN_PROGRESS - 2
+ * TESTING - 4
+ * DONE - 1
+ *
+ * Поступает команда PerformPersonTasks с параметром task_count = 4, что
+ * означает обновление статуса для 3 задач c NEW до IN_PROGRESS и для 1 задачи
+ * с IN_PROGRESS до TESTING.
+ * Если выписать статусы всех задач по порядку, а обновляемые заключить в
+ * квадратные скобки, то изменение можно изобразить следующим образом:
+ *
+ * [NEW, NEW, NEW, IN_PROGRESS,] IN_PROGRESS, TESTING,
+ * TESTING, TESTING, TESTING, DONE
+ *
+ * ==>
+ *
+ * [IN_PROGRESS, IN_PROGRESS, IN_PROGRESS, TESTING,] IN_PROGRESS, TESTING,
+ * TESTING, TESTING, TESTING, DONE
+ *
+ * Таким образом, новые статусы задач будут следующими:
+ *
+ * IN_PROGRESS - 3 обновлённых, 1 старая
+ * TESTING - 1 обновлённая, 4 старых
+ * DONE - 1 старая
+ *
+ * В этом случае необходимо вернуть кортеж из 2 словарей:
+ * - Обновлённые задачи: IN_PROGRESS - 3, TESTING - 1.
+ * - Необновлённые задачи, исключая выполненные: IN_PROGRESS - 1, TESTING - 4.
+ *
+ * Ни один из словарей не должен содержать лишних элементов, то есть статусов,
+ * которым соответствует ноль задач.
+ *
+ * По условию, DONE задачи не нужно возвращать в необновлённых задачах
+ * (untouched_tasks).
+ *
+ * Примечание:
+ *
+ * Обновление словаря одновременно с итерированием по нему может привести к
+ * непредсказуемым последствиям. При возникновении такой необходимости рекомендуется
+ * сначала в отдельном временном словаре собрать информацию об обновлениях, а затем
+ * применить их к основному словарю.
+ *
+ */
+
+/*<=====================================CODE=====================================>*/
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <tuple>
+
+// Перечислимый тип для статуса задачи
+enum class TaskStatus {
+  NEW,          // новая
+  IN_PROGRESS,  // в разработке
+  TESTING,      // на тестировании
+  DONE          // завершена
+};
+
+// Объявляем тип-синоним для map<TaskStatus, int>,
+// позволяющего хранить количество задач каждого статуса
+using TasksInfo = std::map<TaskStatus, int>;
+
+class TeamTasks {
+public:
+  // Получить статистику по статусам задач конкретного разработчика
+  const TasksInfo& GetPersonTasksInfo(const std::string& person) const;
+
+  // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
+  void AddNewTask(const std::string& person);
+
+  // Обновить статусы по данному количеству задач конкретного разработчика,
+  // подробности см. ниже
+  tuple<TasksInfo, TasksInfo> PerformPersonTasks(
+  const std::string& person, int task_count);
+};
+
+/*<-------------------------------------main------------------------------------->*/
+
+int main() {
+  TeamTasks tasks;
+  tasks.AddNewTask("Ilia");
+
+  for (int i = 0; i < 3; ++i) {
+    tasks.AddNewTask("Ivan");
+  }
+
+  std::cout << "Ilia's tasks: ";
+  PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
+  std::cout << "Ivan's tasks: ";
+  PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
+
+  TasksInfo updated_tasks, untouched_tasks;
+
+  tie(updated_tasks, untouched_tasks) =
+      tasks.PerformPersonTasks("Ivan", 2);
+
+  std::cout << "Updated Ivan's tasks: ";
+  PrintTasksInfo(updated_tasks);
+  std::cout << "Untouched Ivan's tasks: ";
+  PrintTasksInfo(untouched_tasks);
+
+  tie(updated_tasks, untouched_tasks) =
+      tasks.PerformPersonTasks("Ivan", 2);
+
+  std::cout << "Updated Ivan's tasks: ";
+  PrintTasksInfo(updated_tasks);
+  std::cout << "Untouched Ivan's tasks: ";
+  PrintTasksInfo(untouched_tasks);
+
+  return 0;
+}
