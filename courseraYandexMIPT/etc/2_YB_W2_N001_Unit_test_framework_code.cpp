eca3ddbb9976077528ee3b73a86115fa2b13b5e1
Nikolai Kutuzov <nik.i.kutuzov@yandex.ru>
2022-07-22 15:22:17 +0400

    upd Код фреймворка юнит-тестов

--- courseraYandexMIPT/etc/2_YB_W2_N001_Unit_test_framework_code.cpp
+++ courseraYandexMIPT/etc/2_YB_W2_N001_Unit_test_framework_code.cpp
@@ -1,13 +1,40 @@
 /*================================================================================*/
 /*                           Unit test framework code                             */
-/*                          Код фреймворка юнит-тестов                            */
 /*================================================================================*/
+#pragma once // included only once
+
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <map>
 #include <set>
 #include <exception>
+
+/*<-----------------------declaration_of_funcs_and_classes----------------------->*/
+
+template <class T>
+std::ostream& operator << (std::ostream& os, const std::set<T>& s);
+
+template <class K, class V>
+std::ostream& operator << (std::ostream& os, const std::map<K, V>& m);
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const std::string& hint);
+
+inline void Assert(bool b, const std::string& hint);
+
+class TestRunner {
+public:
+  template <class TestFunc>
+  void RunTest(TestFunc func, const std::string& test_name);
+
+  ~TestRunner();
+
+private:
+  int fail_count = 0;
+};
+
+/*<----------------------------operators_overloading----------------------------->*/
 
 template <class T>
 std::ostream& operator << (std::ostream& os, const std::set<T>& s) {
@@ -35,6 +62,8 @@ std::ostream& operator << (std::ostream& os, const std::map<K, V>& m) {
   return os << "}";
 }
 
+/*<-----------------------definition_of_funcs_and_classes------------------------>*/
+
 template<class T, class U>
 void AssertEqual(const T& t, const U& u, const std::string& hint)
 {
@@ -47,26 +76,20 @@ void AssertEqual(const T& t, const U& u, const std::string& hint)
 
 inline void Assert(bool b, const std::string& hint) { AssertEqual(b, true, hint); }
 
-class TestRunner {
-public:
-  template <class TestFunc>
-  void RunTest(TestFunc func, const std::string& test_name) {
-    try {
-    func();
-    std::cerr << test_name << " OK" << std::endl;
+template <class TestFunc>
+void TestRunner::RunTest(TestFunc func, const std::string& test_name) {
+  try {
+  func();
+  std::cerr << test_name << " OK" << std::endl;
   } catch (std::runtime_error& e) {
-    ++fail_count;
-    std::cerr << test_name << " fail: " << e.what() << std::endl;
+  ++fail_count;
+  std::cerr << test_name << " fail: " << e.what() << std::endl;
   }
-  }
+}
 
-  ~TestRunner() {
-    if (fail_count > 0) {
-      std::cerr << fail_count << " unit tests failed. Terminate" << std::endl;
-      exit(1);
-    }
+TestRunner::~TestRunner() {
+  if (fail_count > 0) {
+    std::cerr << fail_count << " unit tests failed. Terminate" << std::endl;
+    exit(1);
   }
-
-private:
-  int fail_count = 0;
-};
+}
