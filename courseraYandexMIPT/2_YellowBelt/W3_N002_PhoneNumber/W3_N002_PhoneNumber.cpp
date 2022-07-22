e7d6f42de29f379f7c6447a42c544cf57acc5d8e
Nikolai Kutuzov <nik.i.kutuzov@yandex.ru>
2022-07-22 17:47:41 +0400

    upd proj W3_N002_PhoneNumber - реализовать класс, объявленный в заголовочном файле

--- courseraYandexMIPT/2_YellowBelt/W3_N002_PhoneNumber/W3_N002_PhoneNumber.cpp
+++ courseraYandexMIPT/2_YellowBelt/W3_N002_PhoneNumber/W3_N002_PhoneNumber.cpp
@@ -1,8 +1,8 @@
 /*================================================================================*/
 /*                    C++_Development_Basics: The_Yellow_Belt                     */
 /*================================================================================*/
-/*  Title:                                                                        */
-/*  Start_time:                                                                   */
+/*  Title:  Реализовать класс, объявленный в заголовочном файле                   */
+/*  Start_time: 22.07.2022 16:29                                                  */
 /*  End_time:                                                                     */
 /*  Note:                                                                         */
 /*================================================================================*/
@@ -10,6 +10,14 @@
 /*<===============================Problem_Statement==============================>*/
 
 /*
+ * Вам дан заголовочный файл phone_number.h, содержащий объявление класса
+ * PhoneNumber. При этом в комментариях описано поведение, которое ожидается от
+ * реализации этого класса.
+ *
+ * https://github.com/nikkutuzov/CPP_education/blob/master/courseraYandexMIPT/Templates/2_YB_W3_N002_PhoneNumber/phone_number.h
+ *
+ * Пришлите cpp-файл с определениями методов класса PhoneNumber, которые реализуют
+ * описанное поведение.
  *
  */
 
@@ -17,13 +25,29 @@
 
 #include <iostream>
 
-/*<-----------------------declaration_of_funcs_and_classes----------------------->*/
+#include "phone_number.h"
 
 /*<-------------------------------------main------------------------------------->*/
-
+// ПРИ ОТПРАВКЕ НА ПРОВЕРКУ НУЖНО УДАЛИТЬ ФУНКЦИЮ main()
+// я оставил ее только для проверки компиляции!
 int main() {
 
   return 0;
 }
 
 /*<-----------------------definition_of_funcs_and_classes------------------------>*/
+
+PhoneNumber::PhoneNumber(const std::string &international_number) {}
+
+std::string PhoneNumber::GetCountryCode() const {
+  return country_code_;
+}
+std::string PhoneNumber::GetCityCode() const {
+  return city_code_;
+}
+std::string PhoneNumber::GetLocalNumber() const {
+  return local_number_;
+}
+std::string PhoneNumber::GetInternationalNumber() const {
+  return "+" + country_code_ + "-" + city_code_ + "-" + local_number_;
+}
